eb178405a596140766c6525977077a61e1d00d80
Abhishek Kumar Singh <abhishekkrsingh369@gmail.com>
2023-03-04 11:56:02 +0530

    setting up routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Login from './pages/login/Login'
+import Metamask from './pages/metamask/MetaMask'
+import SignUp from './pages/signup/SignUp'
+import Voter from './pages/voter/Voter'
+import ChairMan from './pages/chairman/ChairMan'
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="metamask" element={<Metamask/>}/>
+          <Route path="signup" element={<SignUp/>}/>
+          <Route path="voter" element={<Voter/>}/>
+          <Route path="chairman" element={<ChairMan/>}/>
+        </Routes>
+      </BrowserRouter>       
     </div>
   );
 }
---
+++ frontend/src/pages/metamask/MetaMask.js
@@ -0,0 +1,9 @@
+
+
+export default function Metamask() {
+    return (
+        <div>
+        <h1>Metamask</h1>
+        </div>
+    )
+}
