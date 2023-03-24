9856dc7b06b0d27800d31ecd9930405a27d9f763
Abhishek Kumar Singh <abhishekkrsingh369@gmail.com>
2023-03-24 10:05:16 +0530

    delete bug

---
+++ frontend/src/pages/voter/CasteVote.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './CastVote.css'
+import {useContext} from 'react'
+import {MetaMaskContext} from '../../context/authContext'
+import {ContractContext} from '../../context/contractContext'
+const CastVote = () => {
+  const {account} = useContext(MetaMaskContext)
+  const {state} = useContext(ContractContext)
+  console.log(state)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <div className='caste-vote'>
+      <h1>CastVote</h1>
+      <h2>Account: {account}</h2>
+      <form onSubmit={(e)=>{handleSubmit(e)}} className='caste-form'>
+        <label>Candidate Name</label>
+        <input type="text" name="candidateName" />
+        <button type="submit">Cast Vote</button>
+      </form>
+    </div>
+  )
+}
+export default CastVote;
