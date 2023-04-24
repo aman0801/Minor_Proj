6fb24b4bb5316c1643bad187c9e4b541d14c2b63
Aman Singh <amansingh20010801@gmail.com>
2023-04-24 14:41:34 +0530

    completed the whole project

--- frontend/src/pages/help/help.js
+++ frontend/src/pages/help/help.js
@@ -1,10 +1,87 @@
+import React, { useState } from 'react';
+import {
+  MDBValidation,
+  MDBValidationItem,
+  MDBInput,
+  MDBInputGroup,
+  MDBCheckbox
+} from 'mdb-react-ui-kit';
 import './help.css';
+import Button from 'react-bootstrap/Button';
+export default function Help() {
+  const [formValue, setFormValue] = useState({
+    fname: '',
+    lname: '',
+    email: '',
+    city: '',
+    state: '',
+    zip: '',
+  });
 
-export default function Help(){
-    return(
-        <div className='help'>
-            <h1>Help</h1>
-            <p>Help</p>
-        </div>
-    );
+  const onChange = (e) => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className='help-page'>
+    <MDBValidation className='row g-3'>
+      <MDBValidationItem  className='col-md-4'>
+        <MDBInput
+          value={formValue.fname}
+          name='fname'
+          onChange={onChange}
+          id='validationCustom01'
+          required
+          label='First name'
+        />
+      </MDBValidationItem>
+      <MDBValidationItem className='col-md-4'>
+        <MDBInput
+          value={formValue.lname}
+          name='lname'
+          onChange={onChange}
+          id='validationCustom02'
+          required
+          label='Last name'
+        />
+      </MDBValidationItem>
+      <MDBValidationItem feedback='Please choose a username.' invalid className='col-md-4'>
+        <MDBInputGroup textBefore='@'>
+          <input
+            type='text'
+            className='form-control'
+            id='validationCustomUsername'
+            placeholder='Username'
+            required
+          />
+        </MDBInputGroup>
+      </MDBValidationItem>
+      <MDBValidationItem className='col-md-6' feedback='Please provide a valid city.' invalid>
+        <MDBInput
+          value={formValue.city}
+          name='city'
+          onChange={onChange}
+          id='validationCustom03'
+          required
+          label='City'
+        />
+      </MDBValidationItem>
+      <MDBValidationItem className='col-md-6' feedback='Please provide a valid zip.' invalid>
+        <MDBInput
+          value={formValue.zip}
+          name='zip'
+          onChange={onChange}
+          id='validationCustom05'
+          required
+          label='Zip'
+        />
+      </MDBValidationItem>
+      <input className='text-area' type='text-area' placeholder='Enter your message here' />
+      <div className='col-12'>
+        <Button variant='dark'>Submit form</Button>
+      
+      </div>
+    </MDBValidation>
+    </div>
+  );
 }
