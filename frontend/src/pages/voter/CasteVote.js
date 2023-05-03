43d19981d612c88d5e27fae48e64d9150505412a
Abhishek Kumar Singh <abhishekkrsingh369@gmail.com>
2023-05-03 12:27:00 +0530

    dropdown

--- frontend/src/pages/voter/CasteVote.js
+++ frontend/src/pages/voter/CasteVote.js
@@ -1,30 +1,36 @@
 import React from 'react'
 import './CastVote.css'
-import {useContext} from 'react'
+import {useContext,useState,useEffect} from 'react'
 import {MetaMaskContext} from '../../context/authContext'
 import {ContractContext} from '../../context/contractContext'
-import {ethers} from 'ethers'
 import Button from 'react-bootstrap/esm/Button'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 const CastVote = () => {
   const {account} = useContext(MetaMaskContext)
   const {state} = useContext(ContractContext)
+  const [selected,setselected] = useState("Please select an option")
+  const [options,setoptions]=useState([])
   const handleSubmit = async (e) => {
     e.preventDefault()
     const{contract} = state;
-    const candidateName = e.target.candidateName.value
-    console.log(contract, candidateName)
-    //const amt = {value: ethers.utils.parseEther('0.001')}
-    const vote = await contract.castVote(candidateName)
+    const candidateName = selected;
+    console.log(candidateName)
+    const vote = await contract.castVote(candidateName);
     await vote.wait()
     console.log("voted")
   }
+  useEffect(()=>{
+    setoptions(['Narendra Modi','Arvinda Kejriwal'])
+  },[])
+  const defaultOption = "Please select an option";
   return (
     <div className='caste-vote'>
       <h1>CastVote</h1>
       <h2>Account: {account}</h2>
       <form onSubmit={(e)=>{handleSubmit(e)}} className='caste-form'>
         <label>Candidate Name</label>
-        <input type="text" name="candidateName" />
+        {options.length>0&&<Dropdown options={options} onChange={(e)=>{setselected(e.value)}} value={defaultOption} placeholder="Select an option" />}
         <Button variant="dark" size="lg" type="submit">Caste Vote</Button>
       </form>
     </div>
